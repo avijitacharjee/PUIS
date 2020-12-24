4ef2c2b18dd21bbd11e4e477af732ad913369cc0
AvijitAcharjee <avijitach@gmail.com>
2020-12-24 11:31:13 +0600

    Initial commit

---
+++ app/src/main/java/com/avijit/puis/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.avijit.puis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl("http://www.example.com")
+        myWebView.settings.javaScriptEnabled = true
+    }
+}
